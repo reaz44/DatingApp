75f78bc99299661159e5309a8dc10ed28a4400b3
Shafkat Reaz <shafkat.reaz1@metlife.com.bd>
2021-03-19 20:47:48 +0600

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -43,6 +43,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,7 +66,9 @@ namespace API
 
             //To route the request in the correct direction
             app.UseRouting();
-
+            
+            //UseCors must be between routing and Endpoint and also prior to authorization and authentication
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
